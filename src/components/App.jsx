041666d5eed1a39d17b8e443680577c9364fdbf1
Ihor Kalyta <ihor.kalyta93@gmail.com>
2023-02-23 23:36:29 +0100

    1.5.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,10 @@
+import { SearchBar } from './Searchbar/Searchbar';
+import { fetchPictureByHits } from '../api';
+import { Component } from 'react';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Button } from './Button/Button';
+import { Spinner } from './Loader/Loader';
+
 export class App extends Component {
   state = {
     images: [],
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,2 +1,15 @@
 import PropTypes from 'prop-types';
 import style from './Button.module.css';
+
+
+import PropTypes from 'prop-types';
+import style from './Button.module.css';
+
+export const Button = ({ onClick }) => {
+  return (
+    <button className={style.Button} type="button" onClick={onClick}>
+      Load More
+    </button>
+  );
+};
+Button.propTypes = { onClick: PropTypes.func };
