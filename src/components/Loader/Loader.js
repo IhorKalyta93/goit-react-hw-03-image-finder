943b47a748ba329f8746534402a74a937f208f42
Ihor Kalyta <ihor.kalyta93@gmail.com>
2023-02-23 21:51:13 +0100

    1.4.1

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -3,7 +3,15 @@ import { Component } from 'react';
 import style from './Loader.module.css';
 
 
-
+export class Spinner extends Component {
+  render() {
+    return (
+      <div className={style.Loader}>
+        <InfinitySpin width="300" color="#3f51b5" />;
+      </div>
+    );
+  }
+}
 
 
 // class App extends Component {
